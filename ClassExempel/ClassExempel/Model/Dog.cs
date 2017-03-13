e008a91a0b5d0d3b11b487c57e889e59e42fd7c0
teamintelliwell <teamintelliwell@gmail.com>
2017-03-13 16:58:25 +0100

    Add files via upload
    Första dagen med Bobby

---
+++ ClassExempel/ClassExempel/Model/Dog.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Synthesis;
+
+namespace ClassExempel.Model
+{
+     class Dog
+    {
+        public string Name;
+       public Gender Gender;
+        public int Age;
+
+
+
+        public string Bark()
+        {
+            string bark = "Woff!";
+
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+
+            synth.SelectVoiceByHints(VoiceGender.Male, VoiceAge.Adult);
+
+            synth.Speak(bark);
+
+            return bark;
+
+
+        
+           
+        } 
+
+    }
+
+    
+
+}
---
+++ ClassExempel/ClassExempel/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassExempel.Model;
+
+
+namespace ClassExempel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Konstruktor finns i alla classer. Classer är blueprints/kakformer. Konstruktorn skapar en kaka/instans
+
+            /*Dog myDog = new Dog();
+            myDog.Name = "Fido";
+            myDog.Gender = Gender.Male;
+            myDog.Age = 7;*/
+
+            Dog myDog = new Dog()
+            {
+
+                Name = "Fido",
+                Gender = Gender.Male,
+                Age = 7,
+
+
+            };
+
+
+
+
+            Console.WriteLine("My dogs name is:" + myDog.Name);
+            Console.WriteLine("My dogs gender is:" + myDog.Gender);
+            Console.WriteLine("My dogs age is:" + myDog.Age);
+            Console.WriteLine(myDog.Bark());
+
+
+
+
+            Cat myCat = new Cat();
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+
+}
